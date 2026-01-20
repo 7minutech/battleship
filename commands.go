e65c7f2232ddf52b6951d6130b821bc93fea505b
7minutech <elijohnson1107@gmail.com>
2026-01-19 20:04:19 -0600

    feat: test command move that takes 2 words

--- commands.go
+++ commands.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
-func runCommand(cmd string) {
-	switch cmd {
+func runCommand(cmds []string) {
+	switch cmds[0] {
 	case "help":
 		help()
 	case "quit":
 		quit()
+	case "move":
+		if len(cmds) != 2 {
+			fmt.Println("move: needs one additonal word for location (move: a1)")
+			return
+		}
+		log.Printf("moving to %s\n", cmds[1])
 	default:
-		fmt.Printf("did not recognize command: %s\n", cmd)
+		fmt.Printf("did not recognize command: %s\n", cmds[0])
 	}
 }
 
