e07d300124f8cd6305c8affc10b42e0d5b2fd076
7minutech <elijohnson1107@gmail.com>
2026-01-21 21:47:14 -0600

    feat: welcome func for username

--- internal/gamelogic/game_commands.go
+++ internal/gamelogic/game_commands.go
@@ -1,34 +1,30 @@
 package gamelogic
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
-	"log"
 	"os"
 )
 
-func gameCommand(cmds []string) {
-	switch cmds[0] {
-	case "help":
-		help()
-	case "quit":
-		quit()
-	case "place":
-		if len(cmds) != 4 {
-			fmt.Println("place: needs 4 args (place s1 a1 a3)")
-			return
-		}
-		log.Printf("placeing ship: %s at start: %s end: %s\n", cmds[1], cmds[2], cmds[3])
-	default:
-		fmt.Printf("did not recognize command: %s\n", cmds[0])
-	}
-}
-
-func help() {
+func Help() {
 	fmt.Println("help: prints possible commands")
 	fmt.Println("quit: exits the program")
 }
 
-func quit() {
+func Quit() {
 	fmt.Println("exiting program...")
 	os.Exit(0)
+}
+
+func Welcome() (string, error) {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Wecome to battle ship please enter your username!")
+	fmt.Print("username: ")
+	scanner.Scan()
+	words := GetWords(scanner.Text())
+	if len(words) == 0 {
+		return "", errors.New("error: did not provide input for user name")
+	}
+	return words[0], nil
 }
