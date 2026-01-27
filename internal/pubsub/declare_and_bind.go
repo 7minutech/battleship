11d99d9212d8da0dff0230ab1ae9a69f41cd75af
7minutech <elijohnson1107@gmail.com>
2026-01-26 22:15:51 -0600

    feat: add DeclareAndBind function for RabbitMQ queue declaration and binding

---
+++ internal/pubsub/declare_and_bind.go
@@ -0,0 +1,43 @@
+package pubsub
+
+import amqp "github.com/rabbitmq/amqp091-go"
+
+type SimpleQueueType int
+
+const (
+	Durabale  SimpleQueueType = 0
+	Transient SimpleQueueType = 1
+)
+
+func DeclareAndBind(conn *amqp.Connection, exchangeName, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+	defer ch.Close()
+
+	queue, err := ch.QueueDeclare(
+		queueName,
+		queueType == Durabale,
+		queueType == Transient,
+		queueType == Transient,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	err = ch.QueueBind(
+		queueName,
+		key,
+		exchangeName,
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, queue, nil
+}
