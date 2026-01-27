cc249e8031e796507289c3c6029cd3e24cc16372
7minutech <elijohnson1107@gmail.com>
2026-01-26 22:41:23 -0600

    feat: establish RabbitMQ connection and implement pause command handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,14 +6,43 @@ import (
 	"os"
 
 	"github.com/7minutech/battleship/internal/gamelogic"
+	"github.com/7minutech/battleship/internal/pubsub"
+	"github.com/7minutech/battleship/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Printf("Failed to connect to RabbitMQ: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to open a channel: %v", err)
+		return
+	}
+	defer ch.Close()
+
+	gamelogic.PrintServerHelp()
+
 	for {
 		scanner := bufio.NewScanner(os.Stdin)
 		fmt.Print(">>> ")
 		scanner.Scan()
 		words := gamelogic.GetWords(scanner.Text())
-		fmt.Print(words)
+		switch words[0] {
+		case "help":
+			gamelogic.Help()
+		case "pause":
+			pubsub.PublishJSON(ch, routing.EXCHANGE_BATTLESHIP_DIRECT, routing.PAUSE_KEY, routing.PauseMessage{Content: "Server is paused"})
+		case "quit":
+			gamelogic.Quit()
+		default:
+			fmt.Printf("did not recognize command: %s\n", words[0])
+		}
 	}
 }
