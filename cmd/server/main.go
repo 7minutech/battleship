b015c7de6cff76d781b16bee340e7cbef8d25d90
7minutech <elijohnson1107@gmail.com>
2026-01-30 12:14:16 -0600

    refactor: simplify command handling logic in main function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -34,21 +34,25 @@ func main() {
 		fmt.Print(">>> ")
 		scanner.Scan()
 		words := gamelogic.GetWords(scanner.Text())
-		switch words[0] {
-		case "help":
-			gamelogic.Help()
-		case "pause":
-			pubsub.PublishJSON(ch, routing.EXCHANGE_BATTLESHIP_DIRECT, routing.PAUSE_KEY, routing.PauseMessage{Content: "Server is paused"})
-		case "quit":
-			gamelogic.Quit()
-		default:
-			fmt.Printf("did not recognize command: %s\n", words[0])
-		}
 
-		// wait for ctrl+c
-		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, os.Interrupt)
-		<-signalChan
-		fmt.Println("RabbitMQ connection closed.")
+		if words[0] == "help" {
+			gamelogic.Help()
+			continue
+		} else if words[0] == "quit" {
+			gamelogic.Quit()
+			break
+		} else if words[0] == "pause" {
+			pubsub.PublishJSON(ch, routing.EXCHANGE_BATTLESHIP_DIRECT, routing.PAUSE_KEY, routing.PauseMessage{Content: "Server is paused"})
+			continue
+		} else {
+			fmt.Printf("did not recognize command: %s\n", words[0])
+			continue
+		}
 	}
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
