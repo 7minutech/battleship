0cfa3180ea9d7b7ef659d177b378f3b8ae605e1f
7minutech <elijohnson1107@gmail.com>
2026-02-02 12:35:17 -0600

    fix: add NEW_PLAYER_KEY constant for player creation event

--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -2,6 +2,7 @@ package routing
 
 const EXCHANGE_BATTLESHIP_DIRECT = "battleship_direct"
 const PAUSE_KEY = "pause_started"
+const NEW_PLAYER_KEY = "player.created"
 
 type PauseMessage struct {
 	Content string
