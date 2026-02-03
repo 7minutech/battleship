455ccc7bb2238d9da42ade35aa88924dba20b10f
7minutech <elijohnson1107@gmail.com>
2026-02-02 22:23:19 -0600

    feat: add message types and constants for show board functionality

--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -3,6 +3,15 @@ package routing
 const EXCHANGE_BATTLESHIP_DIRECT = "battleship_direct"
 const PAUSE_KEY = "pause_started"
 const NEW_PLAYER_KEY = "player.created"
+const SHOW_BOARD_KEY = "board.show"
+const GAME_COMMANDS_QUEUE = "game.commands"
+const GAME_SERVER_KEY = "game.server"
+const BOARD_STATE_KEY = "board.state"
+
+type ShowBoardMessage struct {
+	UserName  string
+	BoardData [][]string
+}
 
 type PauseMessage struct {
 	Content string
