bb4840bb718db649f675937c72d18b7a9b0de69e
7minutech <elijohnson1107@gmail.com>
2026-02-08 20:46:15 -0600

    feat: place ship structs, topic exchange, board place state keys

--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -1,16 +1,32 @@
 package routing
 
 const EXCHANGE_BATTLESHIP_DIRECT = "battleship_direct"
+const EXCHANGE_BATTLESHIP_TOPIC = "battleship_topic"
 const PAUSE_KEY = "pause_started"
 const NEW_PLAYER_KEY = "player.created"
 const SHOW_BOARD_KEY = "board.show"
-const GAME_COMMANDS_QUEUE = "game.commands"
+const GAME_COMMANDS_KEY = "game.commands"
 const GAME_SERVER_KEY = "game.server"
 const BOARD_STATE_KEY = "board.state"
+const PLACE_STATE_KEY = "board.place.state"
+const PLACE_BOARD_KEY = "board.place"
 
 type ShowBoardMessage struct {
 	UserName  string
 	BoardData [][]string
+}
+
+type PlaceShipCommand struct {
+	UserName   string
+	ShipType   string
+	StartCoord string
+	EndCoord   string
+}
+
+type PlaceShipMessage struct {
+	UserName string
+	Success  bool
+	Message  string
 }
 
 type PauseMessage struct {
